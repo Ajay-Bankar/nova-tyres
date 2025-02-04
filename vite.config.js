61b4127a452eaa12db662f43f8e7bbdbd6e6f22d
Ajay-Bankar <bankarajay116@gmail.com>
2025-02-04 13:47:47 +0530

    vite config js file update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',  // Ensure Vite correctly resolves routes
+  server: {
+    historyApiFallback: true, // Redirects unknown routes to index.html
+  },
+});
