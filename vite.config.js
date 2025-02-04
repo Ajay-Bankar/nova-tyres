636553e20d2a95bdbc886fc5d626ab09396026b7
Ajay-Bankar <bankarajay116@gmail.com>
2025-02-04 16:53:23 +0530

    update page not found solution1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Ensure Vite correctly resolves routes
-  server: {
-    historyApiFallback: true, // Redirects unknown routes to index.html
-  },
-});
+})
